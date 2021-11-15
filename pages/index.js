e349499fac5f24e8218478cc60b547f4d95b0d19
Ronnie Rios <ronniesmail14@gmail.com>
2021-11-14 18:52:47 -0600

    basic structure and layout for postdetail

--- pages/index.js
+++ pages/index.js
@@ -9,11 +9,11 @@ export default function Home({ posts }) {
         <title>Ronnie's CMS Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-    <div className="grid-cols-1 lg:grid-cols-12 gap-12">
+    <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
       <div className="lg:col-span-8 col-span-1">
       {posts.map((post) => ( <PostCard post={post.node} key={post.title} /> ))}
       </div>
-        <div className="lg:col:span-4 col-span-1">
+        <div className="lg:col-span-4 col-span-1">
           <div className="lg:sticky relative top-8">
             <PostWidget />
             <Categories />
--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -4,8 +4,21 @@ import { PostDetail, Categories, PostWidget, Author, Comments, CommentsForm } fr
 
 const PostDetails = () => {
     return (
-        <div>
-            PostDetails
+        <div className="container mx-auto px-8 mb-7">
+            <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+                <div className="col-span-1 lg:col-span-8">
+                    <PostDetail />
+                    <Author />
+                    <CommentsForm />
+                    <Comments />
+                </div>
+                <div className="col-span-1 lg:col-span-4">
+                    <div className="relative lg:sticky top-8">
+                        <PostWidget />
+                        <Categories />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
