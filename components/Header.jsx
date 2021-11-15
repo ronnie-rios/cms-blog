3eec03d8af46d0121639e35f5002c6f06df64961
Ronnie Rios <ronniesmail14@gmail.com>
2021-11-14 18:41:08 -0600

    created categories filter

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,13 +1,17 @@
-import React, { useContext } from 'react'
+import React, { useState, useEffect } from 'react';
 
 import Link from 'next/link';
 
-const categories = [
-    {name: 'first', slug: 'first'},
-    {name: 'second', slug: 'second'}, 
-]
+import { getCategories } from '../services';
 
 const Header = () => {
+    const [categories, setCategories] = useState([]);
+
+    useEffect(()=> {
+        getCategories()
+            .then((newCategories) => setCategories(newCategories))
+    }, [])
+
     return (
         <div className="container mx-auto px-10 mb-7">
             <div className="border-b w-full inline-block border-green-700 py-8">
