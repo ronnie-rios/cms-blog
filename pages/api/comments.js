f7f56387a646b77ca27f5593c40ec11bb06c1e54
Ronnie Rios <ronniesmail14@gmail.com>
2021-11-21 17:29:49 -0600

    comment sroutesbackend

--- pages/api/comments.js
+++ pages/api/comments.js
@@ -1,4 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+//any file inside is mapped to  /api/filename and is an endpoint instead of a page
 
 export default function helloAPI(req, res) {
   res.status(200).json({ name: 'John Doe' })
