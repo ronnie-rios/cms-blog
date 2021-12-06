d7780ae96a4d46501a06a6ffc853ce711e6255df
Ronnie Rios <ronniesmail14@gmail.com>
2021-12-05 19:29:35 -0600

    graphql request

--- pages/api/comments.js
+++ pages/api/comments.js
@@ -1,6 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 //any file inside is mapped to  /api/filename and is an endpoint instead of a page
 
-export default function helloAPI(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+import { GraphQLClient } from "graphql-request";
+
+const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
+
+export default function comments( req, res) {
+  const graphQLClient = new GraphQLClient(graphqlAPI, {
+    headers: {
+      authorization: `Bearer ${process.env.}`
+    }
+  })
 }
