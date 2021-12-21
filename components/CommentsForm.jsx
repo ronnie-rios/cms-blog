2e5f460c49aa932ca3da023de3fb42fcda98e182
Ronnie Rios <ronniesmail14@gmail.com>
2021-12-20 21:59:22 -0600

    comments can be submitted for review

--- components/CommentsForm.jsx
+++ components/CommentsForm.jsx
@@ -1,5 +1,7 @@
-import { comment } from 'postcss';
+// import { comment } from 'postcss';
 import React, { useState, useEffect, useRef } from 'react'
+
+import { submitComment } from '../services';
 
 const CommentsForm = ({ slug }) => {
     const [error, setError] = useState(false);
@@ -9,6 +11,11 @@ const CommentsForm = ({ slug }) => {
     const nameEl = useRef();
     const emailEl = useRef();
     const storeDataEl = useRef();
+
+    useEffect(() => {
+        nameEl.current.value = window.localStorage.getItem('name');
+        emailEl.current.value = window.localStorage.getItem('email');
+    }, [])
 
     const handleCommentSubmission = () => {
         setError(false);
@@ -26,17 +33,26 @@ const CommentsForm = ({ slug }) => {
         const commentobj = { name, email, comment, slug }
 
         if (storeData) {
-            localstorage.setItem('name', name);
-            localstorage.setItem('email', email);
+            window.localStorage.setItem('name', name);
+            window.localStorage.setItem('email', email);
         } else {
-            localstorage.removeItem('name', name);
-            localstorage.removeItem('email', email);
+            window.localStorage.removeItem('name', name);
+            window.localStorage.removeItem('email', email);
         }
+
+        submitComment(commentobj)
+            .then((res) => { 
+                setShowSuccessMessage(true);
+            
+                setTimeout(()=> {
+                setShowSuccessMessage(false)
+            }, 3000);
+        })
     }
 
     return (
         <div className="bg-white shadow-lg rounded-lg p-8 pb-8 mb-8">
-            <h3 className="text-xl mb-8 font-semibold border-b pb-4">Comments</h3>
+            <h3 className="text-xl mb-8 font-semibold border-b pb-4">Leave a comment</h3>
             <div className="grid grid-cols-1 gap-4 mb-4">
                 <textarea ref={commentEl}
                 className="p-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-150 bg-gray-110 text-gray-700"
--- pages/api/comments.js
+++ pages/api/comments.js
@@ -1,15 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 //any file inside is mapped to  /api/filename and is an endpoint instead of a page
 
-import { GraphQLClient, gql } from "graphql";
+import { GraphQLClient, gql } from "graphql-request";
 
 const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
+const graphcmsToken = process.env.GRAPH_CMS_TOKEN
 
-export default function comments( req, res) {
+export default async function comments( req, res) {
   const { name, email, slug, comment } = req.body;
   const graphQLClient = new GraphQLClient(graphqlAPI, {
     headers: {
-      authorization: `Bearer ${process.env.GRAPH_CMS_TOKEN}`
+      authorization: `Bearer ${graphcmsToken}`
     }
   })
 
@@ -18,7 +19,7 @@ export default function comments( req, res) {
       createComment(data: {name: $name, email: $email, comment: $comment, post: { connect: { slug: $slug } } } ) { id }
     }
   `
-  const result = await GraphQLClient.request(query, req.body)
+  const result = await graphQLClient.request(query, req.body)
 
-  return res.status(200).send(result)
-}
+  return res.status(200).send(result);
+  }
